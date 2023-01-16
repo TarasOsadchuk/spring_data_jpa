2b615dcba917ba8eda74b00a5ce91018bdbb698e
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-01-16 21:50:40 +0200

    Initial commit

---
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.zaurtregulov.spring.springboot.spring_data_jpa.dao;
+
+import com.zaurtregulov.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByName(String name);
+}
