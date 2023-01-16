7146e6c8aed77e2dd75b729d60c8e9693beafee0
andrejosadcuk <Rudyst.1995@gmail.com>
2023-01-16 21:59:30 +0200

    1

--- src/main/java/com/zaurtregulov/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+
+
     public List<Employee> findAllByName(String name);
 }
